9910c8a83a2de1ec5d4632da403bed4a4fb441d3
Saket Kothari <saketkothari722@gmail.com>
2024-05-01 14:37:43 +0530

    🎉 starting setup

---
+++ 01-essentials/app/layout.js
@@ -0,0 +1,14 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'NextJS Essentials App',
+  description: 'Your first NextJS app!',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
