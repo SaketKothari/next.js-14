cf15ada1012bcb48aa7d02c32d32aadf53a26f84
Saket Kothari <saketkothari722@gmail.com>
2024-05-06 22:37:16 +0530

    optimizing images with nextjs Image component

--- 02-foodies/components/MainHeader.jsx
+++ 02-foodies/components/MainHeader.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 import logoImg from '@/assets/logo.png';
@@ -7,7 +8,7 @@ const MainHeader = () => {
   return (
     <header className={styles.header}>
       <Link className={styles.logo} href="/">
-        <img src={logoImg.src} alt="A plate with food on it" />
+        <Image src={logoImg} alt="A plate with food on it" priority />
         NextLevel Food
       </Link>
 
